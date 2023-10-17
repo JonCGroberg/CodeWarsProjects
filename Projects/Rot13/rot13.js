68e0ccce62c4c1f94fe5845b2b8d9f46e5c128a4
Jonathan <jongroberg@gmail.com>
2023-10-17 17:39:42 -0400

    rot13 finsihed

---
+++ Projects/Rot13/rot13.js
@@ -0,0 +1,52 @@
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function rot13(message) {
+  var encodedMessage = message.split("");
+
+  for (var i = 0; i < message.length; i++) {
+    for (var j = 0; j < alphabet.length; j++) {
+      if (message[i].toLowerCase() == alphabet[j]) {
+        let isUppercase = message[i] == message[i].toUpperCase();
+        let roll = 13 - (alphabet.length - j);
+        if (roll > 0) {
+          encodedMessage[i] = alphabet[roll];
+          if (isUppercase) encodedMessage[i] = encodedMessage[i].toUpperCase();
+        } else {
+          encodedMessage[i] = alphabet[j + 13];
+          if (isUppercase) encodedMessage[i] = encodedMessage[i].toUpperCase();
+        }
+      }
+    }
+  }
+
+  return encodedMessage.join("");
+}
+
+console.log(rot13("Test"));
