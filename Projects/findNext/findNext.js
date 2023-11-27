235955fe3c35b786e9fea5f65f1f51d1d89f8291
Jonathan <jongroberg@gmail.com>
2023-11-27 16:35:58 -0500

    findNext.js

---
+++ Projects/findNext/findNext.js
@@ -0,0 +1,32 @@
+function doEquation(n) {
+  let sum = 0;
+  let digits = n.toString();
+  for (let k = 0; k < digits.length - 1; k++) {
+    let power = digits[k] - digits[k + 1];
+    let number = Math.pow(digits[k], Math.abs(power));
+    let sign = power > 0 ? -1 : 1;
+    sum += sign * number;
+  }
+  sum += Number(digits[digits.length - 1]);
+  return sum;
+}
+
+function findPrev(n) {
+  for (let i = n; i >= 100; i--) {
+    if (doEquation(i) == 0) return i;
+  }
+}
+function findNext(n) {
+  for (let i = n; i < 1000000; i++) {
+    if (doEquation(i) == 0) return i;
+  }
+}
+
+function prevNext(n) {
+  let finalArray = [];
+  let prev = findPrev(n - 1);
+  if (prev) finalArray.push(prev);
+  if (doEquation(n) == 0) finalArray.push(n);
+  finalArray.push(findNext(n + 1));
+  return finalArray;
+}
--- test.js
+++ test.js
@@ -1,37 +1,3 @@
-function doEquation(n) {
-  let sum = 0;
-  let digits = n.toString();
-  for (let k = 0; k < digits.length - 1; k++) {
-    let power = digits[k] - digits[k + 1];
-    let number = Math.pow(digits[k], Math.abs(power));
-    let sign = power > 0 ? -1 : 1;
-    sum += sign * number;
-  }
-  sum += Number(digits[digits.length - 1]);
-  return sum;
-}
+// A rectangle can be split up into a grid of 1x1 squares, the amount of which being equal to the product of the two dimensions of the rectangle. Depending on the size of the rectangle, that grid of 1x1 squares can also be split up into larger squares, for example a 3x2 rectangle has a total of 8 squares, as there are 6 distinct 1x1 squares, and two possible 2x2 squares. A 4x3 rectangle contains 20 squares.
 
-function findPrev(n) {
-  for (let i = n; i >= 100; i--) {
-    if (doEquation(i) == 0) return i;
-  }
-}
-function findNext(n) {
-  for (let i = n; i < 1000000; i++) {
-    if (doEquation(i) == 0) return i;
-  }
-}
-
-function prevNext(n) {
-  let finalArray = [];
-  let prev = findPrev(n - 1);
-  if (prev) finalArray.push(prev);
-  if (doEquation(n) == 0) finalArray.push(n);
-  finalArray.push(findNext(n + 1));
-  return finalArray;
-}
-console.log(doEquation(143));
-console.log(prevNext(150));
-console.log(prevNext(154));
-console.log(prevNext(99));
-console.log(prevNext(100));
+// Your task is to write a function `findSquares` that returns the total number of squares for any given rectangle, the dimensions of which being given as two integers with the first always being equal to or greater than the second.
